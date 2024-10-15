b68b51bd5b28b5755401c23b88c34fa690d970cf
igprad01 <igustiputra99@gmail.com>
2024-10-15 20:45:45 +0700

    add route dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM  from 'react-dom/client'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Template from './Pages/Template'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const route = createBrowserRouter ([
+  {
+    path: "/",
+    element: <Template />
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router = {route} />
+  </React.StrictMode>,
 )
+
