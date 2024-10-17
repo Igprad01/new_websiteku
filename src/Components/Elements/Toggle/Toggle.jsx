5588bcaab153dac2d19f2e40e52011200b48b251
igprad01 <igustiputra99@gmail.com>
2024-10-17 21:13:10 +0700

    fix toggle fontawesome

---
+++ src/Components/Elements/Toggle/Toggle.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
+const Toggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false)
+
+    const handle = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+    return (
+        <button className={`btn btn-square `} onClick={handle}>
+            {isDarkMode ? ( <FontAwesomeIcon icon={faMoon} size="lg" />):(
+                <FontAwesomeIcon icon={faSun} size="lg" />
+            )}
+        </button>
+    )
+}
+
+export default Toggle
