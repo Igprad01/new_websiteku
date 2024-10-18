a0414cf4193dc4f5c12557cd08f6b8aa9e722164
igprad01 <igustiputra99@gmail.com>
2024-10-18 10:56:52 +0700

    fix responsive nav

--- src/Components/Layouts/Navbar/Navbar.jsx
+++ src/Components/Layouts/Navbar/Navbar.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import NavbarList from "../../Elements/NavbarList/NavbarList";
+import Heading1 from "../../Elements/Heading1/Heading1";
 
-const Navbar = (styling) => {
+const Navbar = () => {
+    const [Open,SetOpen] = useState(false);
+
+    const hamburger = () => {
+        SetOpen(!Open);
+    };
     return (
-        <nav className={`w-full p-5 bg-slate-600 text-white`}>
-            <div className={`container mx-auto text-xl `}>
+        <nav className={`w-full p-10 bg-slate-600 text-white`}>
+            <div className={`container mx-auto text-2xl`}>
+                <div className={`hidden justify-between md:flex items-end space-x-5`}>
+                <Heading1 Text = "ini adalah teks"/>
                 <NavbarList />
+                </div>
             </div>
+            <div className={`justify-between md:hidden flex items-end`}>
+                <Heading1 Text= "ini adalah teks" />
+           <button onClick={hamburger}>
+           <svg viewBox="0 0 100 80" width="20" height="20">
+                <rect width="100" height="20" ></rect>
+                <rect y="30" width="100" height="20" ></rect>
+                <rect y="60" width="100" height="20" ></rect>
+            </svg>  
+           </button>
+            </div>
+            {Open && (
+                <div>
+                    <NavbarList />
+                </div>
+            )}
         </nav>
     )
 }
