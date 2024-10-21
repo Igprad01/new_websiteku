c2d57e97009924d018b178160d63b108251933c6
igprad01 <igustiputra99@gmail.com>
2024-10-21 12:00:55 +0700

    on progress hero section

--- src/Components/Layouts/Hero_Section/Hero_section.jsx
+++ src/Components/Layouts/Hero_Section/Hero_section.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Heading1 from "../../Elements/Heading1/Heading1";
 import Heading2 from "../../Elements/Heading2/Heading2";
+import Kalimat from "../../Elements/Paragraph/Paragraph";
 
 const HeroSection= () => {
     return (
-        <div className={`container relative mx-60 my-20 flex justify-between`}>
+        <div className={`container relative mx-60 my-20`}>
             <Heading1 Text = 'ini adalah text' />
             <Heading2 Text = 'ini adalah heading2' />
+          <div>
+          <Kalimat teks = 'ini adalah teks'/>
+          </div>
         </div>
     )
 }
