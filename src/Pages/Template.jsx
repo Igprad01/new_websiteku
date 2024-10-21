65374566627946a3fbb037efbbf5cb0932af2f04
igprad01 <igustiputra99@gmail.com>
2024-10-21 11:32:36 +0700

    add hero_section to template

--- src/Pages/Template.jsx
+++ src/Pages/Template.jsx
@@ -3,12 +3,14 @@ import Navbar from './../Components/Layouts/Navbar/Navbar';
 import "@fontsource/montserrat"; // Defaults to weight 400
 import "@fontsource/montserrat/400.css"; // Specify weight
 import "@fontsource/montserrat/400-italic.css"; // Specify weight and style
+import HeroSection from "../Components/Layouts/Hero_Section/Hero_section";
 
 
 const Template = () => {
     return (
         <body className="font-montserrat">
             <Navbar />
+            <HeroSection />
         </body>
     )
 }
