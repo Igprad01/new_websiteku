fda8461cbc698d6f4c70ff22f81969a06f3d0ce6
igprad01 <igustiputra99@gmail.com>
2024-10-21 11:59:21 +0700

    add components heading2 dan paragraph

---
+++ src/Components/Elements/Heading2/Heading2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Heading2 = ({Text}) => {
+    return (
+        <h2>{Text}</h2>
+    )
+}
+
+export default Heading2
---
+++ src/Components/Elements/Paragraph/Paragraph.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Kalimat = ({teks}) => {
+    return (
+        <p>{teks}</p>
+    )
+}
+
+export default Kalimat
