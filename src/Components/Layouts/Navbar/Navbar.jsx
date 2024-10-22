fdbc8336aecdf961961219dd4994b9fcfa2ef958
igprad01 <igustiputra99@gmail.com>
2024-10-22 10:31:24 +0700

    fix responsive navbar

--- src/Components/Layouts/Navbar/Navbar.jsx
+++ src/Components/Layouts/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import NavbarList from "../../Elements/NavbarList/NavbarList";
-import Toggle from "../../Elements/Toggle/Toggle";
+import Heading1 from "../../Elements/Heading1/Heading1";
 
 const Navbar = () => {
     const [Open,SetOpen] = useState(false);
@@ -9,25 +9,23 @@ const Navbar = () => {
         SetOpen(!Open);
     };
     return (
-        <nav className={`relative flex justify-center items-center text-black my-12 text-2xl`}>
+        <nav className={`flex justify-center items-center text-black my-12 text-2xl`}>
         <div className={`hidden md:flex bg-gray-300 rounded-full shadow-lg px-10 py-5 pl-28 cursor-pointer hover:shadow-black delay-75`}>
             <NavbarList />
         </div>
         <div className={`md:hidden flex justify-end items-end w-full pr-6`}>
-            <button onClick={hamburger}>
+            <button onClick={hamburger} >
                 <svg viewBox="0 0 100 80" width="20" height="20">
-                    <rect width="100" height="20"></rect>
-                    <rect y="30" width="100" height="20"></rect>
-                    <rect y="60" width="100" height="20"></rect>
-                </svg>  
+                  <rect width="100" height="20" fill="white"></rect>
+                  <rect y="30" width="100" height="20" fill="white"></rect>
+                  <rect y="60" width="100" height="20" fill="white"></rect>
+                </svg> 
             </button>
         </div>
         {Open && (
-            <div className={`absolute top-16 bg-slate-50 py-10 px-10 w-full text-left md:hidden`}>
-                <div className={`flex flex-col space-y-4`}>
-                    <NavbarList />
-                </div>
-            </div>
+            <div className="absolute top-20 right-5 left-5 bg-white px-5 py-5 items-center rounded-lg md:hidden">
+                <NavbarList styling={`flex flex-col justify-start space-y-3 text-lg sm:text-xl`} />
+            </div>        
         )}
         {/* <div className="absolute right-32">
             <Toggle />
@@ -38,3 +36,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
