5cf1cbb0db43acd7af2b75b0ce4f5b5e74b8cef6
igprad01 <igustiputra99@gmail.com>
2024-10-22 10:36:47 +0700

    add

--- src/Pages/Template.jsx
+++ src/Pages/Template.jsx
@@ -4,16 +4,16 @@ import "@fontsource/montserrat"; // Defaults to weight 400
 import "@fontsource/montserrat/400.css"; // Specify weight
 import "@fontsource/montserrat/400-italic.css"; // Specify weight and style
 import HeroSection from "../Components/Layouts/Hero_Section/Hero_section";
+import About from "../Components/Layouts/About_Me/About";
 
 
-const Template = () => {
-    return (
-        <body className="font-montserrat">
-            <Navbar />
-            <HeroSection />
-        </body>
-    )
-}
+const Template = () => (
+    <body className="font-montserrat">
+        <Navbar />
+        <HeroSection />
+        <About />
+    </body>
+)
 
 
 export default Template
