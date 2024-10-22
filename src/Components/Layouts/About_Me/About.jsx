ddf4b3baa6cd822b4ca2824c9dbe3e49add3e7f1
igprad01 <igustiputra99@gmail.com>
2024-10-22 11:05:08 +0700

    coming soon responsive herosection and aboutme

--- src/Components/Layouts/About_Me/About.jsx
+++ src/Components/Layouts/About_Me/About.jsx
@@ -4,9 +4,11 @@ import Kalimat from "../../Elements/Paragraph/Paragraph";
 
 const About= () => {
     return (
-        <div className={`container mx-24 my-36`}>
-            <Heading1 Text={'ini adalah teks'} />
-            <Kalimat teks={'ini adalah teks'} />
+        <div className={`container mx-60 my-36`}>
+            <Heading1 Text={'ini adalah teks'} Styling = 'text-center mb-10' />
+            <div className={`text-justify border border-white`}>
+                <Kalimat teks = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' Styling={`px-5 py-2`} /> 
+            </div>
         </div>
     )
 }
--- src/Components/Layouts/Hero_Section/Hero_section.jsx
+++ src/Components/Layouts/Hero_Section/Hero_section.jsx
@@ -5,11 +5,11 @@ import Kalimat from "../../Elements/Paragraph/Paragraph";
 
 const HeroSection= () => {
     return (
-        <div className={`container relative mx-60 my-40`}>
-            <Heading1 Text = 'Putra Pradwi' Styling={`font-bold text-6xl mb-10`} />
-            <Heading2 Text = 'Front End Developer' Styling={`text-9xl mb-10`} />
+        <div className={`container relative mx-10 my-30 md:mx-60 my-40 `}>
+            <Heading1 Text = 'putra pradwi.' Styling={`font-bold text-4xl mb-10 capitalize md:text-5xl`} />
+            <Heading2 Text = 'Front End Developer' Styling={`font-bold text-xl mb-10 md:text-9xl`} />
           <div>
-          <Kalimat teks = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Pellentesque integer bibendum nec magna sit iaculis facilisi? Nec potenti leo netus fames egestas posuere? Ligula erat praesent libero ultrices rutrum. Felis orci vivamus etiam; convallis convallis proin eros scelerisque primis? Class tristique vestibulum augue malesuada nascetur quisque cursus augue. Ex malesuada lacus rutrum nulla convallis tortor non gravida. Per porttitor justo etiam laoreet vivamus vel facilisis ante per. Hac purus neque viverra sem, etiam iaculis ante.' Styling={`font-italic text-xl`}/>
+          <Kalimat teks = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Pellentesque integer bibendum nec magna sit iaculis facilisi? Nec potenti leo netus fames egestas posuere? Ligula erat praesent libero ultrices rutrum. Felis orci vivamus etiam; convallis convallis proin eros scelerisque primis? Class tristique vestibulum augue malesuada nascetur quisque cursus augue. Ex malesuada lacus rutrum nulla convallis tortor non gravida. Per porttitor justo etiam laoreet vivamus vel facilisis ante per. Hac purus neque viverra sem, etiam iaculis ante.' Styling={`font-italic text-xl text-justify`}/>
           </div>
         </div>
     )
