d80667a8a6b0b89b45c7da2be4afd924d881c6c6
igprad01 <igustiputra99@gmail.com>
2024-10-25 10:33:42 +0700

    fix hero_section

--- src/Components/Layouts/Hero_Section/Hero_section.jsx
+++ src/Components/Layouts/Hero_Section/Hero_section.jsx
@@ -4,11 +4,12 @@ import Heading2 from "../../Elements/Heading2/Heading2";
 import Kalimat from "../../Elements/Paragraph/Paragraph";
 
 const HeroSection= () => {
+    const TextRunning = ['']
     return (
         <div className={`container mx-auto px-4 sm:px-6 lg:px-8 py-8`}>
             <div className={`relative`}>
-            <Heading1 Text = 'putra pradwi.' Styling={`font-bold text-2xl mb-10 capitalize sm:text-3xl lg:text-6xl mb-6`} />
-            <Heading2 Text = 'Front End Developer' Styling={`font-bold text-lg mb-10 sm:text-xl lg:text-4xl mb-7`} />
+            <Heading1 Text = 'putra pradwi.' Styling={`font-bold text-2xl mb-10 capitalize sm:text-3xl lg:text-4xl mb-6`} />
+            <Heading2 Text = 'Front End Developer' Styling={`font-bold text-lg mb-10 sm:text-xl lg:text-7xl mb-7`} />
           <div>
           <Kalimat teks = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Pellentesque integer bibendum nec magna sit iaculis facilisi? Nec potenti leo netus fames egestas posuere? Ligula erat praesent libero ultrices rutrum. Felis orci vivamus etiam; convallis convallis proin eros scelerisque primis? Class tristique vestibulum augue malesuada nascetur quisque cursus augue. Ex malesuada lacus rutrum nulla convallis tortor non gravida. Per porttitor justo etiam laoreet vivamus vel facilisis ante per. Hac purus neque viverra sem, etiam iaculis ante.' Styling={`font-italic text-base text-justify sm:text-lg lg:text-xl`}/>
           </div>
