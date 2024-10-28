37d29306c687e832a32fd7cbbf4507d3419206ad
igprad01 <igustiputra99@gmail.com>
2024-10-28 13:05:42 +0700

    add project_section

---
+++ src/Components/Layouts/Project_Section/Project_section.jsx
@@ -0,0 +1,17 @@
+// Section_Project.js
+import React from "react";
+import Card from "../../Elements/Card/Card";
+
+const Section_Project = () => {
+    const card = ['']
+
+    return (
+        <div className="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
+            <div className="grid">
+                <Card Title={'ini adalah title'} Teks={'ini adalah kalimat'}/>
+            </div>
+        </div>
+    );
+};
+
+export default Section_Project;
