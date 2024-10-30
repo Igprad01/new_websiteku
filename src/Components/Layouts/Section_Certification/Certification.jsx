f5d63980b1fa8d6018c5ca61a18ff1f024aaae79
igprad01 <igustiputra99@gmail.com>
2024-10-30 12:07:11 +0700

    fix responsive project section

---
+++ src/Components/Layouts/Section_Certification/Certification.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "../../Elements/Card/Card";
+import Heading1 from "../../Elements/Heading1/Heading1";
+
+const Sertifikat = () => {
+    const Kartu = [
+        {id:1, judul:'ini adalah judul 1', kalimat:'ini adalah kalimat1'},
+        {id:1, judul:'ini adalah judul 1', kalimat:'ini adalah kalimat1'},
+        {id:1, judul:'ini adalah judul 1', kalimat:'ini adalah kalimat1'},
+        {id:1, judul:'ini adalah judul 1', kalimat:'ini adalah kalimat1'}
+
+    ]
+
+    return (
+        <div className={`container mx-auto px-4 py-8 sm:px-6 lg:px-8`}>
+           <Heading1 Text={`ini adalah judul`} Styling={`text-center text-2xl mb-10 capitalize font bold sm:text-3xl md:text-4xl`}/>
+            <div className={`grid grid-cols-1 gap-7`}>
+                {Kartu.map((card)=>(
+                    <Card key={card.id} Title={card.judul} Teks={card.kalimat} />
+                ))}
+            </div>
+        </div>
+
+    )
+    
+}
+
+export default Sertifikat
+
