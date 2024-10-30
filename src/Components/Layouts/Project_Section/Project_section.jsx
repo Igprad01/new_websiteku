29ad1176e163c096b6e150ed5e71e99e86c5d155
igprad01 <igustiputra99@gmail.com>
2024-10-30 12:06:40 +0700

    fix responsive project section

--- src/Components/Layouts/Project_Section/Project_section.jsx
+++ src/Components/Layouts/Project_Section/Project_section.jsx
@@ -1,14 +1,25 @@
 // Section_Project.js
 import React from "react";
 import Card from "../../Elements/Card/Card";
+import Heading1 from "../../Elements/Heading1/Heading1";
 
 const Section_Project = () => {
-    const card = ['']
+    const card = [
+        {id:1, title:'ini adalah teks', teks:'ini adalah teks'},
+        {id:2, title:'ini adalah teks', teks:'ini adalah teks'},
+        {id:3, title:'ini adalah teks', teks:'ini adalah teks'},
+        {id:4, title:'ini adalah teks', teks:'ini adalah teks'},
+        {id:5, title:'ini adalah teks', teks:'ini adalah teks'},
+        {id:6, title:'ini adalah teks', teks:'ini adalah teks'},
+    ]
 
     return (
-        <div className="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
-            <div className="grid">
-                <Card Title={'ini adalah title'} Teks={'ini adalah kalimat'}/>
+        <div className={`container mx-auto px-4 py-8 sm:px-6 lg:px-8 `}>
+            <Heading1 Text={`ini adalah judul`} Styling={`text-center text-2xl mb-10 capitalize font bold sm:text-3xl md:text-4xl`}/>
+            <div className={`grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 `}>
+                {card.map((card)=>(
+                    <Card key={card.id} Title={card.title} Teks={card.teks}/>
+                ))}
             </div>
         </div>
     );
