f9cd0b6aaa13c59b52da7a05a0cf43f6e8dc5e27
igprad01 <igustiputra99@gmail.com>
2024-11-06 11:51:27 +0700

    benerin footer bentar

--- src/Components/Layouts/Footer/Footer.jsx
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Heading1 from "../../Elements/Heading1/Heading1";
 import Heading2 from "../../Elements/Heading2/Heading2";
 import ExternalLink from "../../Elements/ExternalLink/Link";
-
+import Aos from "aos";
+import "aos/dist/aos.css"
+import Image from "../../image/Image";
 const Footer = () =>{
+    useEffect(() => {
+        Aos.init({duration:2000});
+    })
+
     return (
-        <footer className={`mx-auto py-40 bg-slate-500`}>
+        <footer data-aos='fade-up' className={`mx-auto py-40 bg-slate-500`}>
             <div className={`container mx-auto px-4 py-8 sm:px-6 lg:px-8`}>
-                <Heading1 Text={`Mau ngobrol bareng?`} />
-                <div className={`my-36`}>
-                    <Heading2 Text={'ini adalah judul'} />
-                    <ExternalLink konten={'ini adalah konten'} href={''} styling={`text-black`} />
+                <Heading1 Text={`Mau nongki Hingga Berdiskusi ide seru? atau membicarakan terkait sesuatu yang lain?`} Styling={`text-3xl sm:text-6xl md:text-7xl lg:text-7xl`} />
+                <div className={`my-12`}>
+                    <Image src={`/src/image/marin.png`} styleImg={`w-60 rounded-full`}/>
+                    <Heading2 Text={'Contact Me'} Styling={`text-xl mt-5`}/>
+                    <div className={``}>
+                    <ExternalLink konten={'I Gusti Bagus Putra Pradwi'} href={''} styling={`text-black`} />                    </div>
+                    <div className={``}>
+                    <ExternalLink konten={'I Gusti Bagus Putra Pradwi'} href={''} styling={`text-black`} />                    </div>
                 </div>
             </div>
         </footer>
