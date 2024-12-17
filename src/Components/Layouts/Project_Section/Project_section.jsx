3ca3a997f2028b65df8efaa6ba118e2ecc9f2ca2
igprad01 <igustiputra99@gmail.com>
2024-12-17 10:05:57 +0700

    fix responsive web project section

--- src/Components/Layouts/Project_Section/Project_section.jsx
+++ src/Components/Layouts/Project_Section/Project_section.jsx
@@ -10,10 +10,10 @@ const Section_Project = () => {
         Aos.init({duration:2000});
     })
     const card = [
-        {id:1, title:'Portofolio Old', teks:'Project website pertama yang terdiri dari Skills dan beberapa project lama pada website tersebut', img:'/img/newPortofolio.png', stylingklik:'font-bold ml-24 text-xl bg-white', kalimatButton:'ini adalah button'},
-        {id:2, title:'Ucapan Hari Raya Idul Fitri', teks:'Sebuah Project yang dibuat untuk mengucapkan Selamat Hari Raya Idul Fitri, pada website tersebut terdapat beberapa fitur button yang langsung mengarah ke dana kaget, user yang mengakses nya mendapatkan gift THR dari website ini',img:'/img/idulFitri.png'},
-        {id:3, title:'Website Film ', teks:'website film random dengan menggunakan API free film, beberapa fitur masih coming soon yang nantinya update lagi', img:'/img/MoviesWeb.png'},
-        {id:4, title:'Auto Random Color', teks:'project random dengan melihat warna warni pada box yang sudah disediakan', img:'/img/randomColor.png'},
+        {id:1, title:'Portofolio Old', teks:'Project website pertama yang terdiri dari Skills dan beberapa project lama pada website tersebut', img:'/img/newPortofolio.png', stylingklik:'btn btn-primary mx-20 my-24 lg:mx-56', kalimatButton:'ini adalah button'},
+        {id:2, title:'Ucapan Hari Raya Idul Fitri', teks:'Sebuah Project yang dibuat untuk mengucapkan Selamat Hari Raya Idul Fitri, pada website tersebut terdapat beberapa fitur button yang langsung mengarah ke dana kaget, user yang mengakses nya mendapatkan gift THR dari website ini',img:'/img/idulFitri.png', stylingklik:'btn btn-primary mx-20 my-4 lg:mx-56', kalimatButton:'ini adalah button'},
+        {id:3, title:'Website Film ', teks:'website film random dengan menggunakan API free film, beberapa fitur masih coming soon yang nantinya update lagi', img:'/img/MoviesWeb.png', stylingklik:'btn btn-primary mx-20 my-4 lg:mx-56', kalimatButton:'ini adalah button'},
+        {id:4, title:'Auto Random Color', teks:'project random dengan melihat warna warni pada box yang sudah disediakan', img:'/img/randomColor.png', stylingklik:'btn btn-primary mx-20 my-12 lg:mx-56', kalimatButton:'ini adalah button'},
     ]
 
     return (
@@ -29,3 +29,6 @@ const Section_Project = () => {
 };
 
 export default Section_Project;
+
+//  kurang responsif pada card button
+// kurang button pada certificate 
