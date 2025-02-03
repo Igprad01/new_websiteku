fce1b5ab19f56cb3f85acaeddc67b757542ac9e3
igprad01 <igustiputra99@gmail.com>
2025-02-03 16:15:48 +0700

    fix link href

--- src/Components/Elements/Card/Card.jsx
+++ src/Components/Elements/Card/Card.jsx
@@ -3,7 +3,7 @@ import Heading1 from "../Heading1/Heading1";
 import Kalimat from "../Paragraph/Paragraph";
 import Image from "../../image/Image";
 
-const Card = ({Teks,Title,styleHeading,Imgsrc, Ket, styleImage, StyleKalimat,styleButton, textbutton}) => {
+const Card = ({Teks,Title,styleHeading,Imgsrc, Ket, styleImage, StyleKalimat,styleButton, textbutton, Link}) => {
 
     
     return (
@@ -12,7 +12,7 @@ const Card = ({Teks,Title,styleHeading,Imgsrc, Ket, styleImage, StyleKalimat,sty
             <Heading1 Text={Title} Styling={styleHeading} />
             <Image src={Imgsrc} alt={Ket} styleImg={styleImage}/>
             <Kalimat teks={Teks} Styling={StyleKalimat}/>
-            <a href="#">
+            <a href={`${Link}`}>
             <button className={`${styleButton}`}>{textbutton}</button>
             </a>
             </div>
--- src/Components/Layouts/Project_Section/Project_section.jsx
+++ src/Components/Layouts/Project_Section/Project_section.jsx
@@ -10,10 +10,10 @@ const Section_Project = () => {
         Aos.init({duration:2000});
     })
     const card = [
-        {id:1, title:'Portofolio Old', teks:'Project website pertama yang terdiri dari Skills dan beberapa project lama pada website tersebut', img:'/img/newPortofolio.png', stylingklik:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl lg:mx-64 lg:my-32', kalimatButton:'click Here'},
-        {id:2, title:'Ucapan Hari Raya Idul Fitri', teks:'Sebuah Project yang dibuat untuk mengucapkan Selamat Hari Raya Idul Fitri, pada website tersebut terdapat beberapa fitur button yang langsung mengarah ke dana kaget, user yang mengakses nya mendapatkan gift THR dari website ini',img:'/img/idulFitri.png', stylingklik:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl  lg:mx-64 lg:my-10', kalimatButton:'Click Here'},
-        {id:3, title:'Website Film ', teks:'website film random dengan menggunakan API free film, beberapa fitur masih coming soon yang nantinya update lagi', img:'/img/MoviesWeb.png', stylingklik:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl  lg:mx-64 lg:my-10', kalimatButton:'Click here'},
-        {id:4, title:'Auto Random Color', teks:'project random dengan melihat warna warni pada box yang sudah disediakan', img:'/img/randomColor.png', stylingklik:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl  lg:mx-64 lg:my-16 ', kalimatButton:'Click Here'},
+        {id:1, title:'Portofolio Old', teks:'Project website pertama yang terdiri dari Skills dan beberapa project lama pada website tersebut', img:'/img/newPortofolio.png', stylingklik:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl lg:mx-64 lg:my-32', kalimatButton:'click Here', link:'https://www.google.com/'},
+        {id:2, title:'Ucapan Hari Raya Idul Fitri', teks:'Sebuah Project yang dibuat untuk mengucapkan Selamat Hari Raya Idul Fitri, pada website tersebut terdapat beberapa fitur button yang langsung mengarah ke dana kaget, user yang mengakses nya mendapatkan gift THR dari website ini',img:'/img/idulFitri.png', stylingklik:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl  lg:mx-64 lg:my-10', kalimatButton:'Click Here', link:'https://www.google.com/'},
+        {id:3, title:'Website Film ', teks:'website film random dengan menggunakan API free film, beberapa fitur masih coming soon yang nantinya update lagi', img:'/img/MoviesWeb.png', stylingklik:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl  lg:mx-64 lg:my-10', kalimatButton:'Click here', link:'https://www.google.com/'},
+        {id:4, title:'Auto Random Color', teks:'project random dengan melihat warna warni pada box yang sudah disediakan', img:'/img/randomColor.png', stylingklik:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl  lg:mx-64 lg:my-16 ', kalimatButton:'Click Here', link:'https://www.google.com/'},
     ]
 
     return (
@@ -21,7 +21,7 @@ const Section_Project = () => {
             <Heading1 Text={`Project`} Styling={`text-center text-xl capitalize mb-10 sm:text-2xl md:text-4xl lg:text-6xl`}/>
             <div className={`grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 `}>
                 {card.map((card)=>(
-                    <Card key={card.id} Title={card.title} Teks={card.teks} Imgsrc={card.img} styleImage={`w-3/4 mx-auto rounded-xl`} styleHeading={`font-bold text-xl mx-15 mb-5 text-center`} StyleKalimat={`mt-5 mx-0 text-lg sm:mx-20 text-xl md:text-2xl`} styleButton={card.stylingklik} textbutton={card.kalimatButton}/>
+                    <Card key={card.id} Title={card.title} Teks={card.teks} Imgsrc={card.img} styleImage={`w-3/4 mx-auto rounded-xl`} styleHeading={`font-bold text-xl mx-15 mb-5 text-center`} StyleKalimat={`mt-5 mx-0 text-lg sm:mx-20 text-xl md:text-2xl`} styleButton={card.stylingklik} textbutton={card.kalimatButton} Link={card.link}/>
                 ))}
             </div>
         </div>
