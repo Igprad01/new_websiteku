41fa17ec1731318c6d48ef7ac0c0dfc53196e504
igprad01 <igustiputra99@gmail.com>
2025-07-24 13:09:37 +0700

    perbaiki link sertifikasi

--- src/Components/Layouts/Section_Certification/Certification.jsx
+++ src/Components/Layouts/Section_Certification/Certification.jsx
@@ -6,11 +6,11 @@ import "aos/dist/aos.css"
 
 const Sertifikat = () => {
     const Kartu = [
-        {id:1, judul:'Data Analysis', kalimat:'Sertifikasi Penyelesaian Course FreeCodeCamp Data Analysis With Python', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl mt-5', kalimatButton:'Click Here'},
-        {id:2, judul:'Data Analysis - Python Introduction', kalimat:'Sertifikasi Penyelesaian Course Data Analysis With Python DQLab Free Class', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl mt-5', kalimatButton:'Click Here'},
-        {id:3, judul:'SQL Fundamental for Data Science', kalimat:'Sertifikasi penyelesaian Course SQL DQLab Free Class', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-x mt-5', kalimatButton:'CLick Here'},
-        {id:4, judul:'Microsoft Excel Pivot Table', kalimat:'Sertifikasi Penyelesaian Course Excel pivot Table Myskill Id', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl mt-5', kalimatButton:'Click Here'}
-
+        {id:1, judul:'Data Analysis', kalimat:'Sertifikasi Penyelesaian Course FreeCodeCamp Data Analysis With Python', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl mt-5',kalimatButton:'click Here', link:'https://academy.dqlab.id/Certificate_check/result/DQLABFREECLASS01DSEMOH#mycertificate'},
+        {id:2, judul:'Data Analysis - Python Introduction', kalimat:'Sertifikasi Penyelesaian Course Data Analysis With Python DQLab Free Class', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl mt-5', kalimatButton:'Click Here', link:'https://academy.dqlab.id/Certificate_check/result/DQLABINTP1VHLPVW#mycertificate'},
+        {id:3, judul:'SQL Fundamental for Data Science', kalimat:'Sertifikasi penyelesaian Course SQL DQLab Free Class', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-x mt-5', kalimatButton:'CLick Here', link:'https://academy.dqlab.id/Certificate_check/result/DQLABSQLT1RSWSFV#mycertificate'},
+        {id:4, judul:'Microsoft Excel Pivot Table', kalimat:'Sertifikasi Penyelesaian Course Excel pivot Table Myskill Id', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl mt-5', kalimatButton:'Click Here', link:'https://drive.google.com/file/d/1nZ7h6-0pq2u8megQc0dvLTLhHa4mHDPt/view?usp=drive_link'},
+        {id:5, judul:'Data Analysis With Python', kalimat:'Sertifikasi course freecodecamp data analysis with python', buttonstyle:'btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl mt-5', kalimatButton:'Click Here', link:'https://www.freecodecamp.org/certification/Igprad/data-analysis-with-python-v7'}
     ]
 
     useEffect(()=> {
@@ -22,7 +22,7 @@ const Sertifikat = () => {
            <Heading1 Text={`Certification`} Styling={`text-center text-xl mb-10 capitalize sm:text-2xl md:text-4xl lg:text-6xl`}/>
             <div className={`grid grid-cols-1 gap-7`}>
                 {Kartu.map((card)=>(
-                    <Card key={card.id} Title={card.judul} Teks={card.kalimat} styleHeading={`text-3xl`} StyleKalimat={`text-2xl`} styleButton={card.buttonstyle} textbutton={card.kalimatButton}  />
+                    <Card key={card.id} Title={card.judul} Teks={card.kalimat} styleHeading={`text-3xl`} StyleKalimat={`text-2xl`} styleButton={card.buttonstyle} textbutton={card.kalimatButton} Link={card.link}  />
                 ))}
             </div>
         </div>
